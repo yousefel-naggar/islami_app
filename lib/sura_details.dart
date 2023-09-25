ca3e507f42969a68f140dd0944eb41e8858f1a0f
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-09-25 03:23:44 +0300

    sura details

--- lib/sura_details.dart
+++ lib/sura_details.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami/my_theme_data.dart';
 import 'package:islami/tabs/sura_model.dart';
 
-class SuraDetails extends StatelessWidget {
+class SuraDetails extends StatefulWidget {
   static const String routeName = "SuraDetails";
+
+  @override
+  State<SuraDetails> createState() => _SuraDetailsState();
+}
+
+class _SuraDetailsState extends State<SuraDetails> {
+  List<String> verses = [];
 
   @override
   Widget build(BuildContext context) {
     var args = ModalRoute.of(context)?.settings.arguments as SuraModel;
+    if (verses.isEmpty) {
+      loadFile(args.index);
+    }
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
@@ -20,10 +32,41 @@ class SuraDetails extends StatelessWidget {
             style: Theme.of(context).textTheme.bodyLarge,
           ),
         ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Card(
+            elevation: 30,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+                side:
+                    BorderSide(color: MyThemeData.primaryLightColor, width: 2)),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: ListView.builder(
+                itemCount: verses.length,
+                itemBuilder: (context, index) {
+                  return Center(
+                      child: Directionality(textDirection: TextDirection.rtl,
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text("${verses[index]}(${index+1})",
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.bodySmall),
+                        ),
+                      ));
+                },
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
-  loadFile(int ind){
 
+  loadFile(int ind) async {
+    String sura = await rootBundle.loadString("assets/files/${ind + 1}.txt");
+    List<String> lines = sura.split("\n");
+    verses = lines;
+    setState(() {});
   }
 }
