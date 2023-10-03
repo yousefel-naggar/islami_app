c46ff44018fa2c075a1f3a66d563e8888c668496
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-03 11:53:29 +0300

    settings tab init

--- lib/tabs/settings_tab.dart
+++ lib/tabs/settings_tab.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:islami/my_theme_data.dart';
 
 class SettingsTab extends StatefulWidget {
   const SettingsTab({super.key});
@@ -11,70 +10,6 @@ class SettingsTab extends StatefulWidget {
 class _SettingsTabState extends State<SettingsTab> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("language"),
-          InkWell(
-            onTap: () {
-              showLanguageBottomSheet();
-            },
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 25),
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: MyThemeData.primaryLightColor)),
-              child: Text("English"),
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.019,
-          ),
-          Text("Themeing"),
-          InkWell(onTap: () {
-            showThemingBottomSheet();
-          },
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 25),
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: MyThemeData.primaryLightColor)),
-              child: Text("light"),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  showLanguageBottomSheet() {
-    showModalBottomSheet(
-      isScrollControlled: true,
-      shape: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.transparent),
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(15), topRight: Radius.circular(15))),
-      context: context,
-      builder: (context) =>
-          Container(height: MediaQuery.of(context).size.height * 0.6),
-    );
-  }
-  showThemingBottomSheet() {
-    showModalBottomSheet(
-      isScrollControlled: true,
-      shape: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.transparent),
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(15), topRight: Radius.circular(15))),
-      context: context,
-      builder: (context) =>
-          Container(height: MediaQuery.of(context).size.height * 0.6),
-    );
+    return Container(color: Colors.red);
   }
 }
