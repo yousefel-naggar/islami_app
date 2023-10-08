8bf4513b2250ae8fd281a427197a14fa2b2d043d
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-08 17:48:37 +0300

    settings tab init and design

--- lib/tabs/settings_tab.dart
+++ lib/tabs/settings_tab.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami/providers/setting_provider.dart';
+import 'package:provider/provider.dart';
+
+import '../bottom_sheets/language_bottom_sheet.dart';
+import '../bottom_sheets/theming_bottom_sheet.dart';
+import '../my_theme_data.dart';
 
 class SettingsTab extends StatefulWidget {
   const SettingsTab({super.key});
@@ -10,6 +17,70 @@ class SettingsTab extends StatefulWidget {
 class _SettingsTabState extends State<SettingsTab> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red);
+    var provider = Provider.of<SettingsProvider>(context);
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(AppLocalizations.of(context)!.language),
+          InkWell(
+            onTap: () {
+              showLanguageBottomSheet();
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 25),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: MyThemeData.primaryLightColor)),
+              child: Text(provider.local=="en"?"English":"عربي"),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.019,
+          ),
+          Text(AppLocalizations.of(context)!.theming),
+          InkWell(
+            onTap: () {
+              showThemingBottomSheet();
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 25),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: MyThemeData.primaryLightColor)),
+              child: Text("light"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  showLanguageBottomSheet() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      shape: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.transparent),
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15), topRight: Radius.circular(15))),
+      context: context,
+      builder: (context) => LanguageBottomSheet(),
+    );
+  }
+
+  showThemingBottomSheet() {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        shape: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.transparent),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15), topRight: Radius.circular(15))),
+        context: context,
+        builder: (context) => ThemingBottomSheet());
   }
 }
